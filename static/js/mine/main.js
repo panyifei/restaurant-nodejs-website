475492f990e4918a4a822b05130dcadfe060fde3
yifei.pan <981909093@qq.com>
2015-03-20 21:22:59 +0800

    fix

--- static/js/mine/main.js
+++ static/js/mine/main.js
@@ -1,8 +1,6 @@
 //选择某一个菜品的按钮
-$('.c-choose').click(function () {
+$('.j-choose').click(function () {
     $(this).find("i").toggleClass("c-chosen");
-    $(this).parent(".j-item").find('.j-num').toggleClass("hide");
-    $(this).parent(".j-item").find('.j-des').toggleClass("hide")
 });
 
 //加号，减号
@@ -25,24 +23,36 @@ $('.j-mine-btn,.j-back-btn').click(function(){
     $('.j-submit').toggle();
     $('.j-lists').toggle();
     $('.j-orders').toggle();
+    $('.j-lists-header').toggle();
+    $('.j-orders-header').toggle();
     if($(this).hasClass("j-mine-btn")){
-
+        $(".j-orders").empty();
+        $(".j-orders").append($('.c-chosen').parents('.j-item').clone(true));
+        //这里显示和添加数量
+        $('.j-orders .j-item').find('.j-num').toggleClass("hide");
+        $('.j-orders .j-item').find('.j-des').toggleClass("hide");
+        $('.j-orders .j-choose').click(function(){
+            var id =  $(this).parents('.j-item').attr("data-id");
+            $(this).parents('.j-item').remove();
+            $("[data-id='"+id+"']").find(".j-choose").click();
+        });
     }
 });
 
-////提交
-//$(".j-submit").click(function(){
-//    $.ajax({
-//        type: "post",
-//        url: "addOrder",
-//        data: {
-//            telephone: '13121310'
-//        },
-//        success: function () {
-//            //toastr.success('统计状态修改成功');
-//        },
-//        error: function () {
-//            //toastr.error('发生错误');
-//        }
-//    });
-//});
+//提交
+$(".j-submit").click(function(){
+    var order = $('.j-orders .j-item').
+    $.ajax({
+        type: "post",
+        url: "addOrder",
+        data: {
+            telephone: '13121310'
+        },
+        success: function () {
+            //toastr.success('统计状态修改成功');
+        },
+        error: function () {
+            //toastr.error('发生错误');
+        }
+    });
+});
